f2cdbd1c9154fc0dbd418a208a62640e046c88e9
juanfe0479 <felipe0479@hotmail.com>
2022-06-27 10:10:14 -0500

    Adding base functions for all Pages

---
+++ src/test/java/com/pages/HomePage.java
@@ -0,0 +1,79 @@
+package com.pages;
+
+import com.util.pages.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+
+    @FindBy(css = "li a[href*='/abtest']")
+    private WebElement abLink;
+
+    @FindBy(css = "li a[href*='/add_remove']")
+    private WebElement addRemoveLink;
+
+    @FindBy(css = "li a[href*='/basic_auth']")
+    private WebElement loginLink;
+
+    @FindBy(css = "li a[href*='/abtest']")
+    private WebElement abButton;
+
+    @FindBy(css = "li a[href*='/broken_images']")
+    private WebElement brokenImgLink;
+
+    @FindBy(css = "li a[href*='/challenging_dom']")
+    private WebElement challengingDOMLink;
+
+    @FindBy(css = "li a[href*='/checkboxes']")
+    private WebElement checkboxesLink;
+
+    @FindBy(css = "li a[href*='/context_menu']")
+    private WebElement contextMenuLink;
+
+    @FindBy(css = "li a[href*='/digest_auth']")
+    private WebElement digestAuthLink;
+
+    @FindBy(css = "li a[href*='/disappearing_elements']")
+    private WebElement disappearingLink;
+
+    @FindBy(css = "li a[href*='/drag_and_drop']")
+    private WebElement dragDropLink;
+
+    @FindBy(css = "li a[href*='/dropdown']")
+    private WebElement dropdownLink;
+
+    @FindBy(css = "li a[href*='/dynamic_content']")
+    private WebElement dynamicContentLink;
+
+    @FindBy(css = "li a[href*='/dynamic_controls']")
+    private WebElement dynamicControlsLink;
+
+    @FindBy(css = "li a[href*='/dynamic_loading']")
+    private WebElement dynamicLoadingLink;
+
+    @FindBy(css = "li a[href*='/entry_ad']")
+    private WebElement entryAdLink;
+
+    @FindBy(css = "li a[href*='/exit_intent']")
+    private WebElement exitIntentLink;
+
+    @FindBy(css = "li a[href*='/download']")
+    private WebElement downloadLink;
+
+    @FindBy(css = "li a[href*='/upload']")
+    private WebElement uploadLink;
+
+    @FindBy(css = "li a[href*='/floating_menu']")
+    private WebElement floatingMenuLink;
+
+    @FindBy(css = "li a[href*='/forgot_password']")
+    private WebElement forgotPasswordLink;
+
+    @FindBy(css = "li a[href*='/login']")
+    private WebElement formAuthLink;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+}
--- src/test/java/com/util/tests/BaseTest.java
+++ src/test/java/com/util/tests/BaseTest.java
@@ -1,4 +1,27 @@
 package com.util.tests;
 
+import com.drivers.Driver;
+import com.pages.HomePage;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
 public class BaseTest {
+    protected Driver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
+    @Parameters({"browser", "url"})
+    public void start(String browser, String url){
+        driver = new Driver(browser);
+        driver.getDriver().get(url);
+        driver.getDriver().manage().window().maximize();
+        homePage = new HomePage(driver.getDriver());
+    }
+
+    @AfterClass
+    public void cleanUp(){
+        driver.getDriver().manage().deleteAllCookies();
+        driver.getDriver().quit();
+    }
 }
